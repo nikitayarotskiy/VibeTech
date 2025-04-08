4ffa8a3ddb594cee1bdac66ffbb90cb340a12bac
Mossford <Doctorphillip233@gmail.com>
2025-04-07 22:20:35 -0400

    fixed comments

--- Backend/Country.cs
+++ Backend/Country.cs
@@ -5,27 +5,74 @@ namespace Backend
     public class Country
     {
         //economy related
-        public double livingCost { get; set; }//cost of living, dollar value
+        
+        /// <summary>
+        /// cost of living, dollar value
+        /// </summary>
+        public double livingCost { get; set; }
         
         //population related
-        public double fertilityRate { get; set; }//how many citizens are born per month, one int is one citizen
-        public double healthCareStrength { get; set; }//scale of zero to one, zero means that every disease is uncurable, one means that every disease is cured
-        public double diseaseRate { get; set; }//scale of zero to one, zero means that nobody gets diseases, one means that every citizen gets a disease every month
-        public double diseaseStrength { get; set; }//strength of disease from zero to one, zero means that no death chance from disease, one means that every disease causes death
-        public double immigrationRate { get; set; }//how many citizens are moving in per month, one int is once citizen
-        public double migrationRate { get; set; }//how many citizens are moving out per month, one int is once citizen
-        public double mortalityRate { get; set; }//how many citizens are dying per month, one int is once citizen
+        /// <summary>
+        /// how many citizens are born per month, one int is one citizen
+        /// </summary>
+        public double fertilityRate { get; set; }
+        /// <summary>
+        /// scale of zero to one, zero means that every disease is uncurable, one means that every disease is cured
+        /// </summary>
+        public double healthCareStrength { get; set; }
+        /// <summary>
+        /// scale of zero to one, zero means that nobody gets diseases, one means that every citizen gets a disease every month
+        /// </summary>
+        public double diseaseRate { get; set; }
+        /// <summary>
+        /// strength of disease from zero to one, zero means that no death chance from disease, one means that every disease causes death
+        /// </summary>
+        public double diseaseStrength { get; set; }
+        /// <summary>
+        /// how many citizens are moving in per month, one int is once citizen
+        /// </summary>
+        public double immigrationRate { get; set; }
+        /// <summary>
+        /// how many citizens are moving out per month, one int is once citizen
+        /// </summary>
+        public double migrationRate { get; set; }
+        /// <summary>
+        /// how many citizens are dying per month, one int is once citizen
+        /// </summary>
+        public double mortalityRate { get; set; }
         
         //statistic related
-        public double povertyRate { get; set; }//scale of zero to one, where zero is no poverty and one is every citizen is in poverty, if under livingCostPerGdpPerCapita for lots of time then the citizen is added to this number
-        public double crimeRate { get; set; }//scale of zero to one, zero meaning no crime and one meaning each citizen commits a crime every month
+        /// <summary>
+        /// scale of zero to one, where zero is no poverty and one is every citizen is in poverty, if under livingCostPerGdpPerCapita for lots of time then the citizen is added to this number
+        /// </summary>
+        public double povertyRate { get; set; }
+        /// <summary>
+        /// scale of zero to one, zero meaning no crime and one meaning each citizen commits a crime every month
+        /// </summary>
+        public double crimeRate { get; set; }
         
         //internal simulation variables
-        public double population { get; set; }//population of the country, no limit
-        public double gdp { get; set; }//gdp of the country, dollar value, no limit on number
-        public double inflation { get; set; }//inflation as a scalar 1.0f gives no inflation, 1.02f gives 2 percent, etc
-        public double livingCostPerGdpPerCapita { get; set; }//gives a ratio of individual living cost and ratio per household, if it is 1 then you have an equal amount of income and expenses, over 1 and you have a ratio under one you are going in debt, more expenses and less income, if you have over 1 you have more income than expenses
-        public double disasterChance { get; set; }//timer from zero to one for a disaster, it can be increased or decreased by factors in the country, if it hits one we have a disaster
+        /// <summary>
+        /// population of the country, no limit
+        /// </summary>
+        public double population { get; set; }
+        /// <summary>
+        /// gdp of the country, dollar value, no limit on number
+        /// </summary>
+        public double gdp { get; set; }
+        /// <summary>
+        /// inflation as a scalar 1.0f gives no inflation, 1.02f gives 2 percent, etc
+        /// </summary>
+        public double inflation { get; set; }
+        /// <summary>
+        /// gives a ratio of individual living cost and ratio per household, if it is 1 then you have an equal amount of income and expenses, over 1 and you have a ratio under one you are going in debt, more expenses and less income, if you have over 1 you have more income than expenses
+        /// </summary>
+        public double livingCostPerGdpPerCapita { get; set; }
+        /// <summary>
+        /// timer from zero to one for a disaster, it can be increased or decreased by factors in the country, if it hits one we have a disaster
+        /// </summary>
+        public double disasterChance { get; set; }
+        
         //posible disasters: rapid inflation(economic crash), mass disease, mass immigration/migration, cost of living increase, political disaster, etc
 
         //main variables that we will expose to the user to edit
